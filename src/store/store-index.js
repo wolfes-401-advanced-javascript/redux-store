d112f929844d0d6c171ee14d2a3b5341b9e14b42
d-d-wolfe <dave.wolfe623@gmail.com>
2020-08-06 17:21:37 -0700

    removed all the commented out code

--- src/store/store-index.js
+++ src/store/store-index.js
@@ -1,20 +1,10 @@
-// import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// import thunk from 'redux-thunk';
-import products from './products-slice.js'
-// import { categoryReducer } from './categories.js';
-// import products, { fetchProducts } from './products.js';
 
-// let reducers = combineReducers({ categories: categoryReducer, products });
+import products from './products-slice.js'
 
 let rootReducer = combineReducers({ products });
 
 const store = configureStore({ reducer: rootReducer });
-
-// let store = () => {
-//   return createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
-// }
 
 export default store;
